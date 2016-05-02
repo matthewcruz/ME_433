a1c25695c3db06603854500c3d71b46aade18bd1
Matthew Cruz <mattcruz10@gmail.com>
2016-05-02 14:23:07 -0500

    who am i working - imu burned out

--- HW6/HW6.X/i2c_master_noint.c
+++ HW6/HW6.X/i2c_master_noint.c
@@ -125,9 +125,9 @@ unsigned char getWho(){
     unsigned char r = 0x00;
     i2c_master_start(); // make the start bit
     i2c_master_send(0x6B<<1|0); // write the address, shifted left by 1, or'ed with a 0 to indicate writing
-    i2c_master_send(0x0f); // the register to read from
+    i2c_master_send(0x0F); // the register to read from
     i2c_master_restart(); // make the restart bit
-    i2c_master_send(0x6b<<1|1); // write the address, shifted left by 1, or'ed with a 1 to indicate reading
+    i2c_master_send(0x6B<<1|1); // write the address, shifted left by 1, or'ed with a 1 to indicate reading
     r = i2c_master_recv();
     i2c_master_ack(1); // make the ack so the slave knows we got it
     i2c_master_stop(); // make the stop bit
